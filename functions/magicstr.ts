ee4da3729ce69c22a7a77694c01fd5a8ac99edd6
Fennekin The Cute Fox <nathan_zhou@outlook.com>
2022-11-13 00:16:07 -0500

    Update magicstr.ts

--- functions/magicstr.ts
+++ functions/magicstr.ts
@@ -1,4 +1,6 @@
 export async function onRequestGet({ params }) {  
     const res = await fetch(`https://magicstr-gen.pdscir-api.workers.dev/`);  
+    const data = await res.json();  
+    const info = JSON.stringify(data, null, 2);  
     return new Response(res);
 }
