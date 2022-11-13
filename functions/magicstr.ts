581776286ea46f4fc50104d6dc6207a71816ed5c
Fennekin The Cute Fox <nathan_zhou@outlook.com>
2022-11-13 00:24:15 -0500

    Revert "Update magicstr.ts"
    This reverts commit c17a9848d9d1488d88fc5199e4ecfde29dc85900.

--- functions/magicstr.ts
+++ functions/magicstr.ts
@@ -1,9 +1,76 @@
 
 
+const magicStr = [
+    `学习哪有不疯的？硬撑罢了！出不去哪有不疯的？硬撑罢了！没关系 精神稳定一分钟也很厉害了 没精神关系稳定一分钟也很厉害了 没dhdhb精定神稳一分钟也厉很害了  没guan没关系 精稳①分这个发言太过火了，应该立刻向锅中下入葱姜八角香叶，加入生抽，倒入一罐可乐，放盐，盖盖转小火焖十分钟，加水淀粉勾芡，小火收汁，撒上白芝麻，这样一道美味的可乐鸡翅就做好了并且红色是毁灭 蓝色是冷漠 绿色是伪装 白色是虚无 粉色是虚伪 紫色是神秘 橙色是愤怒 黑色是归宿 黄色发给我妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！他奶奶的鸡蛋六舅的哈密瓜妹妹的大窝瓜爷爷的大鸡腿婶婶的大葡萄妈妈的黄瓜菜爸爸的大面包三舅姥爷的大李子二婶的桃子三叔的西瓜七舅姥爷的小荔枝二舅姥爷的火龙果姑姑的猕猴桃祖爷爷的车厘子祖姥爷的大菠萝祖奶奶的大榴莲二爷的小草莓他三婶姥姥的大白菜他哥哥的大面条妹妹的小油菜弟弟的西葫芦姐姐的大土豆姐夫的大青椒爷爷的大茄子嗯啊，杀杀杀！！好可怕杀杀杀杀杀杀上勾拳！下勾拳！左勾拳！右勾拳！扫堂腿！回旋踢！这是蜘蛛吃耳屎，这是龙卷风摧毁停车场！这是羚羊蹬，这是山羊跳！乌鸦坐飞机！老鼠走迷宫！大象踢腿！愤怒的章鱼！巨斧砍大树！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！`
+    ,`We re no strangers to love
+  You know the rules and so do I (do I)
+  A full commitment s what I m thinking of
+  You wouldn t get this from any other guy
+  I just wanna tell you how I m feeling
+  Gotta make you understand
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  We ve known each other for so long
+  Your heart s been aching, but you re too shy to say it (say it)
+  Inside, we both know what s been going on (going on)
+  We know the game and we re gonna play it
+  And if you ask me how I m feeling
+  Don t tell me you re too blind to see
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  We ve known each other for so long
+  Your heart s been aching, but you re too shy to say it (to say it)
+  Inside, we both know what s been going on (going on)
+  We know the game and we re gonna play it
+  I just wanna tell you how I m feeling
+  Gotta make you understand
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you
+  Never gonna give you up
+  Never gonna let you down
+  Never gonna run around and desert you
+  Never gonna make you cry
+  Never gonna say goodbye
+  Never gonna tell a lie and hurt you`
+    ,`起来就看到这么多作业死死死死死死死死死死死死死死死死死死`
+    ,`Happy Birthday to You
+  Happy Birthday to You
+  Happy Birthday Dear (programmer)
+  Happy Birthday to You.`
+  ,`其实大家长期生活在高压的环境下，情绪有点小问题是正常哒～一定学会自我调节，要相信一切都会啊啊啊啊啊啊啊啊啊啊啊饿啊啊啊啊啊啊啊啊啊啊！哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈！fjfuyfcjufbjigbu！啥！砂！杀！沙！纱！煞！鲨！刹！痧！霎！榝！殺！歃！硰！好起来的哒！～加油（≧∇≦） 我的精神挺好的呀 我的神好挺的精呀 挺精呀我的好的 精挺好我的神的呀 我好的精神的呀 的好的呀上勾拳！下勾拳！左勾拳！扫堂腿！回旋踢！蜘蛛吃耳屎，龙卷风摧毁停车场！羚羊蹬，山羊跳！乌鸦坐飞机！老鼠走迷宫！大象踢腿！愤怒的章鱼！巨斧砍大树！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！！！！！
+  ！`,
+  `彻底疯狂。、，，、呃呃呃！呃呃！左钩拳！上钩拳！。喵喵拳。，！呃呃呃！！头脑风暴.？！，、呃呃！呃呃！黑暗能量..。！呃呃啊啊！`
+  ]
+  
+  function get_random (list) {
+    return list[Math.floor((Math.random()*list.length))];
+  }
+  
 
 export async function onRequestGet({ params }) {  
-    const res = await fetch(`https://magicstr-gen.pdscir-api.workers.dev/`);  
-    const data = await res.json();  
-    const info = JSON.stringify(data, null, 2);  
-    return new Response(info);
+    return new Response(get_random(magicStr));
 }
