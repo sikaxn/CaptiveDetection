a225f636e2a89eff9b840dd8ad276dde0f9e9d7e
Fennekin The Cute Fox <nathan_zhou@outlook.com>
2023-02-16 14:01:27 -0500

    dont fix it.

---
+++ functions/magicstr.ts
@@ -0,0 +1,85 @@
+
+
+const magicStr = [
+  `学习哪有不疯的？硬撑罢了！出不去哪有不疯的？硬撑罢了！没关系 精神稳定一分钟也很厉害了 没精神关系稳定一分钟也很厉害了 没dhdhb精定神稳一分钟也厉很害了  没guan没关系 精稳①分这个发言太过火了，应该立刻向锅中下入葱姜八角香叶，加入生抽，倒入一罐可乐，放盐，盖盖转小火焖十分钟，加水淀粉勾芡，小火收汁，撒上白芝麻，这样一道美味的可乐鸡翅就做好了并且红色是毁灭 蓝色是冷漠 绿色是伪装 白色是虚无 粉色是虚伪 紫色是神秘 橙色是愤怒 黑色是归宿 黄色发给我妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！妈的，忍不了，一拳把地球打爆！他奶奶的鸡蛋六舅的哈密瓜妹妹的大窝瓜爷爷的大鸡腿婶婶的大葡萄妈妈的黄瓜菜爸爸的大面包三舅姥爷的大李子二婶的桃子三叔的西瓜七舅姥爷的小荔枝二舅姥爷的火龙果姑姑的猕猴桃祖爷爷的车厘子祖姥爷的大菠萝祖奶奶的大榴莲二爷的小草莓他三婶姥姥的大白菜他哥哥的大面条妹妹的小油菜弟弟的西葫芦姐姐的大土豆姐夫的大青椒爷爷的大茄子嗯啊，杀杀杀！！好可怕杀杀杀杀杀杀上勾拳！下勾拳！左勾拳！右勾拳！扫堂腿！回旋踢！这是蜘蛛吃耳屎，这是龙卷风摧毁停车场！这是羚羊蹬，这是山羊跳！乌鸦坐飞机！老鼠走迷宫！大象踢腿！愤怒的章鱼！巨斧砍大树！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！`
+  ,`We re no strangers to love
+You know the rules and so do I (do I)
+A full commitment s what I m thinking of
+You wouldn t get this from any other guy
+I just wanna tell you how I m feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+We ve known each other for so long
+Your heart s been aching, but you re too shy to say it (say it)
+Inside, we both know what s been going on (going on)
+We know the game and we re gonna play it
+And if you ask me how I m feeling
+Don t tell me you re too blind to see
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+We ve known each other for so long
+Your heart s been aching, but you re too shy to say it (to say it)
+Inside, we both know what s been going on (going on)
+We know the game and we re gonna play it
+I just wanna tell you how I m feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you`
+  ,`起来就看到这么多作业死死死死死死死死死死死死死死死死死死`
+  ,`Happy Birthday to You
+Happy Birthday to You
+Happy Birthday Dear (programmer)
+Happy Birthday to You.`
+,`其实大家长期生活在高压的环境下，情绪有点小问题是正常哒～一定学会自我调节，要相信一切都会啊啊啊啊啊啊啊啊啊啊啊饿啊啊啊啊啊啊啊啊啊啊！哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈！fjfuyfcjufbjigbu！啥！砂！杀！沙！纱！煞！鲨！刹！痧！霎！榝！殺！歃！硰！好起来的哒！～加油（≧∇≦） 我的精神挺好的呀 我的神好挺的精呀 挺精呀我的好的 精挺好我的神的呀 我好的精神的呀 的好的呀上勾拳！下勾拳！左勾拳！扫堂腿！回旋踢！蜘蛛吃耳屎，龙卷风摧毁停车场！羚羊蹬，山羊跳！乌鸦坐飞机！老鼠走迷宫！大象踢腿！愤怒的章鱼！巨斧砍大树！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！彻底疯狂！！！！！
+！`,
+`彻底疯狂。、，，、呃呃呃！呃呃！左钩拳！上钩拳！。喵喵拳。，！呃呃呃！！头脑风暴.？！，、呃呃！呃呃！黑暗能量..。！呃呃啊啊！`,
+'想不想要爸爸的大消息啊 你说啊 告诉我想不想要爸爸的消息塞满你的小聊天框 想要是吧 把聊天框打开让爸爸狠狠发送消息进去',
+'我草好喜欢撕咬想到撕咬我就心花怒放兴高采烈心静自然凉心香瓣心不两用心所谓危心无二想心头刺眼中疔心病还须心上医心病还用心药医心存高官志在巨富心不应口心如木石心如金石心平气定心狠手毒心驰魏阙心地正直心殒胆落心腹重患心回意转心领意会心神恍惚心神不定心花怒发心术不端心术不正心不由意心不由主心急如火心慌意急心腹之病心同止水心头鹿撞心腹之疾心慈面软心忙意急心贯白日心焦火燎心焦如火心悦神怡心怡神旷心痒难揉心摹手追心腹之忧心广体胖心殒胆破心荡神怡心若死灰心惊胆颤心醉魂迷心焦如焚心荡神摇心旌摇摇心甘情原心浮气盛心浮气粗心如刀搅心绪如麻心腹大患心如刀绞心细于发心手相忘心烦虑乱心灰意懒心惊胆丧心谤腹非心往神驰心惊肉跳心里有鬼心如刀到心满原足心如刀锉心如坚石心满愿足心烦意冗心如刀锯心慌意乱心慌撩乱心辣手狠心悬两地心寒胆落心寒胆战心痒难抓心孤意怯心弛神往心满意得心折首肯心闲手敏心惊肉战心惊胆落心惊胆战心痒难挝心劳意攘心劳意冗心照情交心胆俱碎心忙意乱心惊胆怕心旷神飞心旷神怡',
+`Fennekin is so cute`,
+`火狐狸太可爱了`,
+`Evan is so cute`
+,`What's for dinner?`  
+,`PHP is the best language in the world`
+]
+
+
+  
+  function get_random (list) {
+    return list[Math.floor((Math.random()*list.length))];
+  }
+  
+
+export async function onRequestGet({ params }) {  
+    return new Response(get_random(magicStr));
+}
